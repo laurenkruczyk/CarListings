f5909ebf1893827214c3b1464911b60e293872f1
lauren.kruczyk <lauren.kruczyk@gmail.com>
2014-04-14 12:05:55 -0400

    second feature complete

---
+++ app/models/manufacturer.rb
@@ -0,0 +1,5 @@
+class Manufacturer < ActiveRecord::Base
+  has_many :car_listings, dependent: :nullify
+  validates :name, presence: true
+  validates :country, presence: true
+end
---
+++ spec/features/salesman_records_manufacturer_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+feature 'recording a manufacturer', %q{
+As a car salesperson
+I want to record a car manufacturer
+So that I can keep track of the types of cars found in the lot
+} do
+
+#Acceptance Criteria:
+
+# I must specify a manufacturer name and country.
+# If I do not specify the required information, I am presented with errors.
+# If I specify the required information, the manufacturer is recorded and I am redirected to enter another new manufacturer.
+
+  scenario 'creating a manufacturer' do
+    visit new_manufacturer_path
+    fill_in 'Name', with: 'Ford'
+    select 'United States', from: 'Country'
+
+    click_on 'Create Manufacturer'
+    expect(page).to have_content('Successfully created manufacturer')
+    expect(current_path).to eql(new_manufacturer_path)
+  end
+end
